1bfdc40f44bb81770d1ca1bf8fe8ad1fb54afeb2
Spyros <koukas@lms.mech.upatras.gr>
2020-11-24 20:29:09 +0200

    Test that demonstrates Futures usage and client statuses, created a future based client

---
+++ src/test/java/com/github/rosjava_actionlib/ClientServerStatusTest.java
@@ -0,0 +1,168 @@
+/**
+ * Copyright 2020 Spyros Koukas
+ *
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.rosjava_actionlib;
+
+import actionlib_tutorials.FibonacciActionFeedback;
+import actionlib_tutorials.FibonacciActionGoal;
+import actionlib_tutorials.FibonacciActionResult;
+import eu.test.utils.RosExecutor;
+import eu.test.utils.TestProperties;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.After;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+import org.ros.RosCore;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Focus on {@link FutureBasedClient} status changes
+ * Demonstrate future usage
+ */
+public class ClientServerStatusTest {
+    private static final Logger LOGGER= LogManager.getLogger(ClientServerStatusTest.class);
+
+    private static final TestProperties testProperties = TestProperties.getFromDefaultFile();
+
+    private static final String ROS_HOST_IP = testProperties.getRosHostIp();
+    private static final int ROS_MASTER_URI_PORT = testProperties.getRosMasterUriPort();
+    private static final String ROS_MASTER_URI = testProperties.getRosMasterUri();
+    private RosCore rosCore = null;
+
+    private FutureBasedClient futureBasedClient = null;
+    private SimpleServer simpleServer = null;
+    private final RosExecutor rosExecutor = new RosExecutor(ROS_HOST_IP);
+
+    @Before
+    public void before() {
+        try {
+            this.rosCore = RosCore.newPublic(ROS_MASTER_URI_PORT);
+            this.rosCore.start();
+            this.rosCore.awaitStart(testProperties.getRosCoreStartWaitMillis(), TimeUnit.MILLISECONDS);
+
+            this.simpleServer = new SimpleServer();
+            this.futureBasedClient = new FutureBasedClient();
+
+            this.rosExecutor.startNodeMain(this.simpleServer, this.simpleServer.getDefaultNodeName().toString(), ROS_MASTER_URI);
+            this.simpleServer.waitForStart();
+            this.rosExecutor.startNodeMain(this.futureBasedClient, this.futureBasedClient.getDefaultNodeName().toString(), ROS_MASTER_URI);
+            final boolean serverStarted = this.futureBasedClient.waitForServerConnection(30);
+            Assume.assumeTrue("Server Not Started", serverStarted);
+        } catch (final Exception er3) {
+            LOGGER.error(ExceptionUtils.getStackTrace(er3));
+            Assume.assumeNoException(er3);
+
+        }
+
+    }
+
+    /**
+     * Demonstrates status of Client
+     */
+    @Test
+    public void testFutureBasedClientWithStatuses() {
+        try {
+            LOGGER.trace("Starting Tasks");
+            this.futureBasedClient.waitForServerConnection(20);
+            LOGGER.trace("Server Started");
+            final ActionFuture<FibonacciActionGoal, FibonacciActionFeedback, FibonacciActionResult> resultFuture = this.futureBasedClient.invoke(3);
+            String lastClientState = null;
+            while (!resultFuture.isDone()) {
+                final String clientState = resultFuture.getCurrentState().name();
+                if (lastClientState != clientState) {
+                    LOGGER.trace("Client State:" + clientState);
+                    lastClientState = clientState;
+                }
+                try {
+                    Thread.sleep(1);
+                } catch (final Exception er3) {
+                    LOGGER.error(ExceptionUtils.getStackTrace(er3));
+                }
+            }
+            final String clientState = resultFuture.getCurrentState().name();
+            LOGGER.trace("Client State:" + clientState);
+
+            //This is blocking
+            final FibonacciActionResult result = resultFuture.get();
+            LOGGER.trace("Result:" + result.getResult().getSequence());
+        } catch (final Exception e) {
+            LOGGER.error(ExceptionUtils.getStackTrace(e));
+        }
+    }
+
+    /**
+     * Demonstrate future based result getting
+     */
+    @Test
+    public void testFutureBasedClient() {
+        try {
+            LOGGER.trace("Starting Tasks");
+            this.futureBasedClient.waitForServerConnection(20);
+            LOGGER.trace("Server Started");
+            final ActionFuture<FibonacciActionGoal, FibonacciActionFeedback, FibonacciActionResult> resultFuture = this.futureBasedClient.invoke(3);
+
+            //This is blocking anyway
+            final FibonacciActionResult result = resultFuture.get();
+            LOGGER.trace("Result:" + result.getResult().getSequence());
+        } catch (final Exception e) {
+            LOGGER.error(ExceptionUtils.getStackTrace(e));
+        }
+    }
+
+    @After
+    public void after() {
+        try {
+            rosExecutor.stopNodeMain(simpleServer);
+        } catch (final Exception e2) {
+            LOGGER.error(ExceptionUtils.getStackTrace(e2));
+        }
+        try {
+            rosExecutor.stopNodeMain(futureBasedClient);
+        } catch (final Exception e2) {
+            LOGGER.error(ExceptionUtils.getStackTrace(e2));
+        }
+
+
+        try {
+            if (this.rosExecutor != null) {
+                this.rosExecutor.stopAllNodesAndClose();
+            }
+        } catch (final Exception e) {
+            LOGGER.error(ExceptionUtils.getStackTrace(e));
+        }
+
+        try {
+            if (this.rosCore != null) {
+                this.rosCore.shutdown();
+
+            }
+        } catch (final Exception e) {
+            LOGGER.error(ExceptionUtils.getStackTrace(e));
+        }
+
+        this.futureBasedClient = null;
+        this.simpleServer = null;
+        this.rosCore = null;
+    }
+
+}
---
+++ src/test/java/com/github/rosjava_actionlib/FutureBasedClient.java
@@ -0,0 +1,160 @@
+/**
+ * Copyright 2020 Spyros Koukas
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.rosjava_actionlib;
+
+import actionlib_msgs.GoalStatus;
+import actionlib_msgs.GoalStatusArray;
+import actionlib_tutorials.*;
+import com.google.common.base.Stopwatch;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.ros.message.Duration;
+import org.ros.namespace.GraphName;
+import org.ros.node.AbstractNodeMain;
+import org.ros.node.ConnectedNode;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Class to demonstrate/test the action futures
+ *
+ * @author Spyros Koukas
+ */
+class FutureBasedClient extends AbstractNodeMain implements ActionClientListener<FibonacciActionFeedback, FibonacciActionResult> {
+    private static Log LOGGER = LogFactory.getLog(SimpleClient.class);
+    private ActionClient actionClient = null;
+
+    private volatile boolean isStarted = false;
+
+
+    @Override
+    public GraphName getDefaultNodeName() {
+        return GraphName.of("fibonacci_future_client");
+    }
+
+
+    /**
+     * @param seconds the maximum time to wait before the client is started
+     *
+     * @return
+     */
+    public final boolean waitForServerConnection(final double seconds) {
+        final Stopwatch stopwatch = Stopwatch.createStarted();
+        while (!this.isStarted && stopwatch.elapsed(TimeUnit.SECONDS) <= seconds) {
+            this.sleep(100);
+        }
+        if (this.isStarted) {
+            final Duration serverTimeout = new Duration(Math.max(0.1, seconds - stopwatch.elapsed(TimeUnit.SECONDS)));
+            boolean serverStarted = false;
+            LOGGER.trace("Waiting for action server to start...");
+            serverStarted = this.actionClient.waitForActionServerToStart(serverTimeout);
+            if (serverStarted) {
+                LOGGER.trace("Action server started.\n");
+                return true;
+            } else {
+                LOGGER.trace("No actionlib server found after waiting for " + serverTimeout.totalNsecs() / 1e9 + " seconds!");
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * @param order
+     *
+     * @return a future of the fibonacci
+     */
+    public final ActionFuture<FibonacciActionGoal, FibonacciActionFeedback, FibonacciActionResult> invoke(final int order) {
+        // Create Fibonacci goal message
+        final FibonacciActionGoal goalMessage = (FibonacciActionGoal) actionClient.newGoalMessage();
+        final FibonacciGoal fibonacciGoal = goalMessage.getGoal();
+        // set Fibonacci parameter
+        fibonacciGoal.setOrder(order);
+        LOGGER.trace("Sending goal for order:" + order);
+        final ActionFuture<FibonacciActionGoal, FibonacciActionFeedback, FibonacciActionResult> result = actionClient.sendGoal(goalMessage);
+        return result;
+    }
+
+
+    @Override
+    public void onStart(final ConnectedNode node) {
+        this.actionClient = new ActionClient<FibonacciActionGoal, FibonacciActionFeedback, FibonacciActionResult>(node, "/fibonacci", FibonacciActionGoal._TYPE, FibonacciActionFeedback._TYPE, FibonacciActionResult._TYPE);
+        this.isStarted = true;
+        // Attach listener for the callbacks
+        this.actionClient.addListener(this);
+
+    }
+
+    /**
+     * @param message
+     */
+    @Override
+    public void resultReceived(final FibonacciActionResult message) {
+        FibonacciResult result = message.getResult();
+        int[] sequence = result.getSequence();
+        int i;
+
+
+        LOGGER.trace("Got Fibonacci result sequence: ");
+        for (i = 0; i < sequence.length; i++)
+            LOGGER.trace(Integer.toString(sequence[i]) + " ");
+        LOGGER.trace("");
+    }
+
+    /**
+     * @param message
+     */
+    @Override
+    public void feedbackReceived(final FibonacciActionFeedback message) {
+        FibonacciFeedback result = message.getFeedback();
+        int[] sequence = result.getSequence();
+        int i;
+
+        LOGGER.trace("Feedback from Fibonacci server: ");
+        for (i = 0; i < sequence.length; i++)
+            LOGGER.trace(Integer.toString(sequence[i]) + " ");
+        LOGGER.trace("\n");
+    }
+
+    /**
+     * @param status The status message received from the server.
+     */
+    @Override
+    public void statusReceived(final GoalStatusArray status) {
+        if (LOGGER.isInfoEnabled()) {
+            final List<GoalStatus> statusList = status.getStatusList();
+            for (final GoalStatus goalStatus : statusList) {
+                LOGGER.info("GoalID: " + goalStatus.getGoalId().getId() + " GoalStatus: " + goalStatus.getStatus() + " - " + goalStatus.getText());
+            }
+            LOGGER.info("Goal Status: " + this.actionClient.getGoalState());
+        }
+    }
+
+    /**
+     * @param msec
+     */
+    private final void sleep(final long msec) {
+        try {
+            Thread.sleep(msec);
+        } catch (InterruptedException ex) {
+        }
+    }
+
+}
