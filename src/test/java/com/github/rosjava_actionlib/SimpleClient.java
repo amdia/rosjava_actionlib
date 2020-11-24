18df4d53e44c59a2d097aaad1456dd199067fc24
Spyros <koukas@lms.mech.upatras.gr>
2020-11-24 20:27:35 +0200

    StartTasks restructured, code broke down in methods, logger turned into Log4j2

--- src/test/java/com/github/rosjava_actionlib/SimpleClient.java
+++ src/test/java/com/github/rosjava_actionlib/SimpleClient.java
@@ -21,7 +21,6 @@ import actionlib_msgs.GoalID;
 import actionlib_msgs.GoalStatus;
 import actionlib_msgs.GoalStatusArray;
 import actionlib_tutorials.*;
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.ros.message.Duration;
@@ -35,17 +34,10 @@ import java.util.List;
 /**
  * Class to test the actionlib client.
  *
- * @author Ernesto Corbellini ecorbellini@ekumenlabs.com
  * @author Spyros Koukas
  */
 class SimpleClient extends AbstractNodeMain implements ActionClientListener<FibonacciActionFeedback, FibonacciActionResult> {
-    static {
-        // comment this line if you want logs activated
-        System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
-    }
-
     private static Log LOGGER = LogFactory.getLog(SimpleClient.class);
-
     private ActionClient actionClient = null;
     private volatile boolean resultReceived = false;
     private volatile boolean isStarted = false;
@@ -58,35 +50,37 @@ class SimpleClient extends AbstractNodeMain implements ActionClientListener<Fibo
 
 
     /**
-     * Getter for isStarted
      *
-     * @return isStarted
-     **/
-    public void waitForStart() {
-        while (!this.isStarted) {
-            try {
-                Thread.sleep(5);
-            } catch (final InterruptedException ie) {
-                LOGGER.error(ExceptionUtils.getStackTrace(ie));
-            } catch (final Exception e) {
-                LOGGER.error(ExceptionUtils.getStackTrace(e));
+     * @param seconds the maximum time to wait before the client is started
+     * @return
+     */
+    public  final boolean waitForServerConnection(final double seconds) {
+
+        if (this.isStarted) {
+            final Duration serverTimeout = new Duration(seconds);
+            boolean serverStarted = false;
+            LOGGER.trace("Waiting for action server to start...");
+            serverStarted = this.actionClient.waitForActionServerToStart(serverTimeout);
+            if (serverStarted) {
+                LOGGER.trace("Action server started.\n");
+                return true;
+            } else {
+                LOGGER.trace("No actionlib server found after waiting for " + serverTimeout.totalNsecs() / 1e9 + " seconds!");
+                return false;
             }
+        } else {
+            return false;
         }
+
     }
 
+
+    /**
+     * Demonstrates using the client in a node
+     * This is a rather simple set of sequential calls.
+     */
     public void startTasks() {
-        Duration serverTimeout = new Duration(20);
-        boolean serverStarted;
-
-
-        // Attach listener for the callbacks
-        actionClient.addListener(this);
-        System.out.println("\nWaiting for action server to start...");
-        serverStarted = actionClient.waitForActionServerToStart(new Duration(20));
-        if (serverStarted) {
-            System.out.println("Action server started.\n");
-        } else {
-            System.out.println("No actionlib server found after waiting for " + serverTimeout.totalNsecs() / 1e9 + " seconds!");
+        if(!this.waitForServerConnection(20)){
             System.exit(1);
         }
 
@@ -95,68 +89,75 @@ class SimpleClient extends AbstractNodeMain implements ActionClientListener<Fibo
         final FibonacciGoal fibonacciGoal = goalMessage.getGoal();
         // set Fibonacci parameter
         fibonacciGoal.setOrder(3);
-        System.out.println("Sending goal...");
+        LOGGER.trace("Sending goal...");
         actionClient.sendGoal(goalMessage);
+
         final GoalID gid1 = goalMessage.getGoalId();
-        System.out.println("Sent goal with ID: " + gid1.getId());
-        System.out.println("Waiting for goal to complete...");
+        LOGGER.trace("Sent goal with ID: " + gid1.getId());
+        LOGGER.trace("Waiting for goal to complete...");
         while (actionClient.getGoalState() != ClientState.DONE) {
             sleep(1);
         }
-        System.out.println("Goal completed!\n");
+        LOGGER.trace("Goal completed!\n");
 
-        System.out.println("Sending a new goal...");
+        LOGGER.trace("Sending a new goal...");
         actionClient.sendGoal(goalMessage);
         final GoalID gid2 = goalMessage.getGoalId();
-        System.out.println("Sent goal with ID: " + gid2.getId());
-        System.out.println("Cancelling this goal...");
+        LOGGER.trace("Sent goal with ID: " + gid2.getId());
+        LOGGER.trace("Cancelling this goal...");
         actionClient.sendCancel(gid2);
         while (actionClient.getGoalState() != ClientState.DONE) {
             sleep(1);
         }
-        System.out.println("Goal cancelled succesfully.\n");
-        System.out.println("Bye!");
+        LOGGER.trace("Goal cancelled successfully.\n");
+        LOGGER.trace("Bye!");
 
     }
 
 
     @Override
-    public void onStart(ConnectedNode node) {
-        actionClient = new ActionClient<FibonacciActionGoal, FibonacciActionFeedback, FibonacciActionResult>(node, "/fibonacci", FibonacciActionGoal._TYPE, FibonacciActionFeedback._TYPE, FibonacciActionResult._TYPE);
-
+    public void onStart(final ConnectedNode node) {
+        this.actionClient = new ActionClient<FibonacciActionGoal, FibonacciActionFeedback, FibonacciActionResult>(node, "/fibonacci", FibonacciActionGoal._TYPE, FibonacciActionFeedback._TYPE, FibonacciActionResult._TYPE);
         this.isStarted = true;
+        // Attach listener for the callbacks
+        this.actionClient.addListener(this);
 
-//        System.exit(0);
     }
 
+    /**
+     * @param message
+     */
     @Override
-    public void resultReceived(FibonacciActionResult message) {
+    public void resultReceived(final FibonacciActionResult message) {
         FibonacciResult result = message.getResult();
         int[] sequence = result.getSequence();
         int i;
 
         resultReceived = true;
-        System.out.print("Got Fibonacci result sequence: ");
+        LOGGER.trace("Got Fibonacci result sequence: ");
         for (i = 0; i < sequence.length; i++)
-            System.out.print(Integer.toString(sequence[i]) + " ");
-        System.out.println("");
+            LOGGER.trace(Integer.toString(sequence[i]) + " ");
+        LOGGER.trace("");
     }
 
+    /**
+     * @param message
+     */
     @Override
-    public void feedbackReceived(FibonacciActionFeedback message) {
+    public void feedbackReceived(final FibonacciActionFeedback message) {
         FibonacciFeedback result = message.getFeedback();
         int[] sequence = result.getSequence();
         int i;
 
-        System.out.print("Feedback from Fibonacci server: ");
+        LOGGER.trace("Feedback from Fibonacci server: ");
         for (i = 0; i < sequence.length; i++)
-            System.out.print(Integer.toString(sequence[i]) + " ");
-        System.out.print("\n");
+            LOGGER.trace(Integer.toString(sequence[i]) + " ");
+        LOGGER.trace("\n");
     }
 
     @Override
     public void statusReceived(final GoalStatusArray status) {
-        if(LOGGER.isInfoEnabled()) {
+        if (LOGGER.isInfoEnabled()) {
             List<GoalStatus> statusList = status.getStatusList();
             for (GoalStatus gs : statusList) {
                 LOGGER.info("GoalID: " + gs.getGoalId().getId() + " -- GoalStatus: " + gs.getStatus() + " -- " + gs.getText());
@@ -167,9 +168,10 @@ class SimpleClient extends AbstractNodeMain implements ActionClientListener<Fibo
 
     /**
      *
+     *
      * @param msec
      */
-    void sleep(final long msec) {
+    private final void sleep(final long msec) {
         try {
             Thread.sleep(msec);
         } catch (InterruptedException ex) {
